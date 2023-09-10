a3de9deef437c64035e62c4f7415e000698963e4
Mohannad Ragab <mohannadragab53@gmail.com>
2023-09-10 16:23:53 +0300

    remove firebase from index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ import logging from './start/logging.js';
 import production from './start/prod.js';
 import configuration from './start/config.js';
 import issues from './start/issues.js';
-import functions from 'firebase-functions';
 
 const sDebugger = debug('app:startup');
 const app = express();
@@ -24,4 +23,3 @@ const port = config.get('port') || 3000;
 const server = app.listen(port, () => sDebugger(`Listening on port ${port}...`));
 
 export default server;
-export const api = functions.https.onRequest(app);
